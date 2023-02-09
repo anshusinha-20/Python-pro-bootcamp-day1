ac4072f0a516dec8c7eb5f027ca2baf25672aa24
Anshu Sinha <anshujuly2@gmail.com>
2023-02-09 10:51:37 +0530

    Add day1 main.py file

---
+++ main.py
@@ -0,0 +1,8 @@
+# Day1 Project: Band name generator
+
+print("Welcome to the Band Name Generator!")
+city = input("Enter the name of the city: ")
+print(city)
+petName = input("Enter the name of the pet: ")
+print(petName)
+print(f"Your band name could be {city} {petName}")
