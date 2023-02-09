933a2a3fd3eeb00f966ee5e2ac02f3de2d2b6121
Anshu Sinha <anshujuly2@gmail.com>
2023-02-09 10:05:51 +0530

    Add day1 main.py file

---
+++ main.py
@@ -0,0 +1,16 @@
+# name = input("What is your name? ")
+# print(name)
+
+# print(f"Hello {input('What is your name?')}")
+# print(len("Anshu Sinha"))
+
+####################################
+
+# Day 1 Project: Band name generator
+
+print("Welcome to the Band Name Generator.")
+city = input("Enter the name of your city: ")
+print(city)
+petName = input("Enter the name of your pet: ")
+print(petName)
+print(f"Your band name could be {city} {petName}")
